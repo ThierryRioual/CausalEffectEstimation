09f7ac07e6f105ef9ddba5c9e8e46ebc2a800768
Pierre-Henri Wuilemin <pierre-henri.wuillemin@lip6.fr>
2024-08-16 10:47:50 +0200

    pdfized

--- pdfize.sh
+++ pdfize.sh
@@ -1,6 +1,14 @@
-
 CONVERTPDF="jupyter nbconvert --embed-images --template=widelab --to webpdf --output-dir=outputs"
 CONVERTPDFNOCODE="$CONVERTPDF --no-input --no-prompt --output={notebook_name}-nocode"
 
-$CONVERTPDF 1-Potential\ Outcome\ and\ Randomized\ Control\ Trial/Lab1pyAgrum.ipynb
-$CONVERTPDFNOCODE 1-Potential\ Outcome\ and\ Randomized\ Control\ Trial/Lab1pyAgrum.ipynb
+$CONVERTPDF notebooks/pyAgrumLab1a.ipynb  
+$CONVERTPDF notebooks/pyAgrumLab1b.ipynb  
+$CONVERTPDF notebooks/pyAgrumLab2.ipynb   
+$CONVERTPDF notebooks/pyAgrumLab4.ipynb   
+$CONVERTPDF notebooks/pyAgrumLab5.ipynb
+
+$CONVERTPDFNOCODE notebooks/pyAgrumLab1a.ipynb  
+$CONVERTPDFNOCODE notebooks/pyAgrumLab1b.ipynb  
+$CONVERTPDFNOCODE notebooks/pyAgrumLab2.ipynb   
+$CONVERTPDFNOCODE notebooks/pyAgrumLab4.ipynb   
+$CONVERTPDFNOCODE notebooks/pyAgrumLab5.ipynb
